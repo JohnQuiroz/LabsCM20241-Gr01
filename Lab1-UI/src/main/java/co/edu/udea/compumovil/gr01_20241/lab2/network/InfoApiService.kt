68d3b232e2c6a16a533ea27fe6a3614fe90b0c6f
John Quiroz <bayron.quiroz@udea.edu.co>
2024-04-23 22:20:10 -0500

    Se conecta a un API/Rest

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr01_20241/lab2/network/InfoApiService.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr01_20241/lab2/network/InfoApiService.kt
@@ -1,13 +1,38 @@
 package co.edu.udea.compumovil.gr01_20241.lab2.network
 
+import javax.net.ssl.*
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
+import java.security.cert.X509Certificate
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
 private const val BASE_URL = "https://demo6267984.mockable.io/"
 
+val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
+    override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
+    override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
+    override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
+})
+
+fun getUnsafeOkHttpClient(): OkHttpClient.Builder {
+    val sslContext = SSLContext.getInstance("SSL")//.init(null, trustAllCerts, java.security.SecureRandom())
+    sslContext.init(null, trustAllCerts, java.security.SecureRandom())
+
+    val sslSocketFactory = sslContext.getSocketFactory()
+    return OkHttpClient.Builder()
+        .sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
+        .hostnameVerifier { _, _ -> true }
+}
+
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(ScalarsConverterFactory.create())
+    .client(
+        getUnsafeOkHttpClient().build()
+        )
     .baseUrl(BASE_URL)
     .build()
 
